71386f343985514b3bc8d713cd64ca3636ed17bd
selvinakash <108287199+selvinakash@users.noreply.github.com>
2022-12-04 20:47:56 +0530

    Update app.js
    code changes

--- app.js
+++ app.js
@@ -1,38 +1,53 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.filepath;
-      var newpath = './uploads' + files.filetoupload.originalFilename;
-      fs.readFile(oldpath, function (err, files) {
-        if (err) throw err;
-        console.log('File read!');
-        
-        // Write the file
-        fs.writeFile(newpath, files, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-        console.log('File written!');
+const express = require('express');         // Express Web Server
+const busboy = require('connect-busboy');   // Middleware to handle the file upload https://github.com/mscdex/connect-busboy
+const path = require('path');               // Used for manipulation with path
+const fs = require('fs-extra');             // Classic fs
+ 
+const app = express(); // Initialize the express web server
+app.use(busboy({
+    highWaterMark: 2 * 1024 * 1024, // Set 2MiB buffer
+})); // Insert the busboy middle-ware
+ 
+const uploadPath = path.join(__dirname, './uploads/'); // Register the upload path
+fs.ensureDir(uploadPath); // Make sure that he upload path exits
+ 
+ 
+/**
+ * Create route /upload which handles the post request
+ */
+app.route('/upload').post((req, res, next) => {
+ 
+    req.pipe(req.busboy); // Pipe it trough busboy
+ 
+    req.busboy.on('file', (fieldname, file, filename) => {
+        console.log(`Upload of '${filename.filename}' started`);
+ 
+        // Create a write stream of the new file
+        const fstream = fs.createWriteStream(path.join(uploadPath, filename.filename));
+        // Pipe it trough
+        file.pipe(fstream);
+ 
+        // On finish of the upload
+        fstream.on('close', () => {
+            console.log(`Upload of '${filename.filename}' finished`);
+            res.redirect('back');
         });
-        
-        // Delete the file
-        fs.unlink(oldpath, function (err) {
-        if (err) throw err;
-        console.log('File deleted!');
-        });
-        });
- });
-  } else {
+    });
+});
+ 
+ 
+/**
+ * Serve the basic index.html with upload form
+ */
+app.route('/').get((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<form action="upload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="fileToUpload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
-  }
-}).listen(2000);
+});
+ 
+const server = app.listen(2000, function () {
+    console.log(`Listening on port ${server.address().port}`);
+});
